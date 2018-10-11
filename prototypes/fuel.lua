b98c76bd6a89fbc54526df4fbf4a50134ff463e3
The Horscht <realhorscht@gmail.com>
2018-10-11 21:37:13 +0200

    added function to parse fuel value

--- prototypes/fuel.lua
+++ prototypes/fuel.lua
@@ -8,15 +8,39 @@ pollution.icon = "__choke-on-smoke__/graphics/icons/fluid/pollution.png"
 pollution.icon_size = 32
 data:extend{pollution}
 
+-- Converts a string like "225MJ" to a number which represents the megajoules
+-- 2MJ wooden chest nuclear fuel 1.21GJ
+function parse_joule_string_to_mjs(joule_str)
+  local amount, unit = string.match(joule_str, "^(.+)([kKMGTPEZY]J)$")
+  amount = tonumber(amount)
+  if amount == nil or unit == nil then
+    return nil
+  end
+  local powers_of_ten = {
+    kJ = -3, KJ = -3,
+    MJ = 0,
+    GJ = 3,
+    TJ = 6,
+    PJ = 9,
+    EJ = 12,
+    ZJ = 15,
+    YJ = 18,
+  }
+  amount = amount * 1 * 10^powers_of_ten[unit]
+  return amount
+end
+
 -- Make everything thats fuel burnable in our special furnace
 for k,v in pairs(data.raw.item) do
   if v.fuel_category == "chemical" then
+    log(v.name ..": fuel_value parsed = ".. parse_joule_string_to_mjs(v.fuel_value))
+    log(v.name ..": energy_required = ".. (1 * parse_joule_string_to_mjs(v.fuel_value)))
     data:extend{
       {
         type = "recipe",
         name = _(v.name.."-to-smoke"),
         category = _("fuel"),
-        energy_required = 5,
+        energy_required = 1 * parse_joule_string_to_mjs(v.fuel_value),
         emissions_multiplier = 1,
         hide_from_stats = true,
         hidden = true,
